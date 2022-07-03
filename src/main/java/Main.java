6243c1c64135f81d71ced8e65432ef6f2551676c
myke2424 <myke2424@gmail.com>
2022-07-03 15:13:09 -0400

    Brute force get request.. manually construct uri

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Main {
+    public static void main(String[] args) {
+        Search search = new Search.Builder("Merge two dictionaries")
+                .site("stackoverflow")
+                .accepted()
+                .build();
+
+        StackExchange stackExchange = new StackExchange();
+        stackExchange.testRequest("Merge two dictionaries");
+    }
 }
--- src/main/java/StackExchange.java
+++ src/main/java/StackExchange.java
@@ -1,3 +1,11 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.URI;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+
 public class StackExchange implements Searchable {
     private static final String SEARCH_ENDPOINT = "/search/advanced";
     private static final String ANSWERS_ENDPOINT = "/answers/endpoint";
@@ -21,5 +29,50 @@ public class StackExchange implements Searchable {
     @Override
     public void search(String query) {
 
+    }
+
+    // TODO: Should the Http client be behind an interface... and injected in constructor?
+    private HttpRequest getRequest(String url, HashMap<String, String> queryParams) {
+        String uri = url + "?";
+        int i = 0;
+        int mapSize = queryParams.size();
+
+        // Manually construct uri with query params...
+        for (var entry : queryParams.entrySet()) {
+            uri += (entry.getKey() + "=" + entry.getValue().replaceAll("\\s+", ""));
+            if (i < mapSize - 1) {
+                uri += "&";
+            }
+            i++;
+        }
+
+        System.out.println("URI: " + uri);
+
+        HttpRequest request;
+        try {
+            request = HttpRequest.newBuilder()
+                    .uri(new URI(uri))
+                    .GET()
+                    .build();
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Invalid Request...");
+        }
+
+        if (request == null) {
+            throw new IllegalStateException("Invalid Request... null");
+        }
+
+        return request;
+
+    }
+
+    public void testRequest(String query) {
+        var params = new HashMap<String, String>();
+        params.put("q", query);
+        params.put("accepted", "true");
+        params.put("site", "stackoverflow");
+        this.getRequest(this.searchUrl, params);
     }
 }
