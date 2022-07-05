f6129c2bc0b80e28cc6fc2b966a0743e60e4db05
myke2424 <myke2424@gmail.com>
2022-07-05 19:24:36 -0400

    Throw filenotfound if config file is invalid

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,20 +1,31 @@
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Map;
 
+/**
+ * App configuration class
+ */
 public class Config {
 
-    // TODO: Catch exception if file isn't found and log msg
-    public static Map<String, Object> load(String filename) {
+    /**
+     * Load YAML configuration file
+     *
+     * @param filename YAML config file located in src/main/resources/<config.yaml>
+     * @return Map representation of the config
+     */
+    public static Map<String, Object> load(String filename) throws FileNotFoundException {
         InputStream inputStream =
                 Config.class.getClassLoader().getResourceAsStream(filename);
 
+        if (inputStream == null) {
+            throw new FileNotFoundException("Config file doesn't exist... exiting");
+        }
         Yaml yaml = new Yaml();
 
-        Map<String, Object> data = yaml.load(inputStream);
-        System.out.println(data);
+        Map<String, Object> config = yaml.load(inputStream);
 
-        return data;
+        return config;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
+import java.io.FileNotFoundException;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        var config = Config.load("config.yaml");
+        try {
+            var config = Config.load("configz.yaml");
+        } catch (FileNotFoundException e) {
+            LOGGER.info(e.getMessage());
+            System.exit(1);
+        }
+
 
         var arguments = Commands.parseArgs(args);
 
