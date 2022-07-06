b1d01d58a9a39fdcede4da2f145e4e04ed2ad5c1
myke2424 <myke2424@gmail.com>
2022-07-06 10:46:59 -0400

    Remove catch from http client, propagate errors up the stack instead

--- src/main/java/Http.java
+++ src/main/java/Http.java
@@ -1,6 +1,4 @@
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
@@ -8,11 +6,13 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Map;
 import java.util.logging.Logger;
-import java.util.zip.GZIPInputStream;
 
-// TODO: Should the Http client be behind an interface... and injected in constructor?
-// No need to hide this behind an interface, stdlib http client is good.
-public class Http {
+
+interface RequestSender {
+    HttpResponse get(String url, Map<String, String> params) throws IOException, InterruptedException, URISyntaxException;
+}
+
+public final class Http implements RequestSender {
     private static final Logger LOGGER = Logger.getLogger(Http.class.getName());
 
     private HttpClient client;
@@ -21,13 +21,12 @@ public class Http {
         this.client = HttpClient.newHttpClient();
     }
 
-    // TODO: Should params be optional or generic hashmap/
-    private String buildURI(String url, Map<String, String> params) {
+    private String buildUri(String url, Map<String, String> params) {
         String uri = url + "?";
         int i = 0;
         int mapSize = params.size();
 
-        // Manually construct uri with query params...
+        // Manually construct uri with query params
         for (var entry : params.entrySet()) {
             uri += (entry.getKey() + "=" + entry.getValue().replaceAll("\\s+", ""));
             if (i < mapSize - 1) {
@@ -39,66 +38,28 @@ public class Http {
         return uri;
     }
 
-    // Maybe add headers? We don't need them for stackexchange API however.
+
     private HttpRequest buildRequest(String url, Map<String, String> params) throws URISyntaxException {
-        String uri = this.buildURI(url, params);
+        String uri = this.buildUri(url, params);
         LOGGER.info(String.format("Making request to URI: %s", uri));
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI(uri))
-                .GET()
-                .build();
+        HttpRequest request = HttpRequest.newBuilder().uri(new URI(uri)).GET().build();
 
 
         return request;
     }
 
-    private String parseGzip(HttpResponse<InputStream> response) throws IOException {
-        var gzip = new GZIPInputStream(response.body());
 
-
-        // Read gzip into buffer
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        byte[] buffer = new byte[4096];
-        int length;
-        while ((length = gzip.read(buffer)) > 0) {
-            outStream.write(buffer, 0, length);
-
-        }
-
-        String bytes = new String(outStream.toByteArray(), "UTF-8");
-        return bytes;
+    private HttpResponse sendRequest(HttpRequest request, HttpResponse.BodyHandler handler) throws IOException, InterruptedException {
+        HttpResponse response = this.client.send(request, handler);
+        return response;
     }
 
 
-    private String sendRequest(HttpRequest request) throws IOException,
-            InterruptedException {
-        var response = this.client.send(request,
+    public HttpResponse get(String url, Map<String, String> params) throws IOException, InterruptedException, URISyntaxException {
+        HttpRequest request = this.buildRequest(url, params);
+        HttpResponse response = this.sendRequest(request,
                 HttpResponse.BodyHandlers.ofInputStream());
-
-        // TODO: Might make sense to move parsing gzip into stack exchange class
-        // Since gzip is an implementation detail for stack exchange.
-        return this.parseGzip(response);
-
-    }
-
-
-    public String get(String url, Map<String, String> params) {
-        String response = null;
-        try {
-            HttpRequest request = this.buildRequest(url, params);
-            response = this.sendRequest(request);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            System.out.println("Failed to make http request...");
-            System.exit(0);
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
         return response;
     }
